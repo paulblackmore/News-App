005ab01b43d6baf87b1afd75adde77f6c34b63e9
Paul <paul@live-eo.com>
2021-01-16 13:20:38 +0100

    added slect and input to side panel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import classes from './SidePanel.module.css';
 
+const countries = [
+	'ae', 
+	'ar', 
+	'at', 
+	'au', 
+	'be', 
+	'bg', 
+	'br', 
+	'ca', 
+	'ch', 
+	'cn', 
+	'co'
+]
+
 const SidePanel = () => {
+
 	return(
     <div className={classes.container}>
-			<button className={classes.btn}>Headlines</button>
+			<input 
+				placeholder="Search articles..."
+			/>
+			<select name="countries">
+				{countries.map((country, index) => 	{
+					return(
+						<option 
+							key={index} 
+							value={country}
+						>{country}</option>
+					)
+				})}
+			</select>
+			<button 
+				className={classes.btn}
+			>Headlines</button>
 		</div>
 	)
 }
