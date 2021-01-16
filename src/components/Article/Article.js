ca7e1d64950c1ae631921fc2b8b2db49e131cc54
Paul <paul@live-eo.com>
2021-01-16 20:31:27 +0100

    changed article container to a href

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,7 +3,7 @@ import classes from './Article.module.css';
 
 const Article = ({ article }) => {
   return(
-    <div className={classes.container}>
+    <a href={article.url} className={classes.container}>
       <img src={article.urlToImage ? article.urlToImage : 'https://via.placeholder.com/350'} />
       <header>
         <span>{article.author ? article.author : 'Anonymous'}</span>
@@ -13,8 +13,8 @@ const Article = ({ article }) => {
         <h3>{article.title ? article.title : 'No Title'}</h3>
         <p>{article.description ? article.description : 'No Description'}</p>
       </div>
-      <a href={article.url}>View Article</a>
-    </div>
+      {/* <a href={article.url}>View Article</a> */}
+    </a>
   )
 }
 
