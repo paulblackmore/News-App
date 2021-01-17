e288fbb79eba67bcd5ca9cf48a4196b8e8bf9894
Paul <paul@live-eo.com>
2021-01-17 18:24:21 +0100

    merged search-articles

--- src/service/service.js
+++ src/service/service.js
@@ -1,8 +1,22 @@
-const url = 'https://newsapi.org/v2/top-headlines';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-export function getHeadlines(country) {
+export const newsService = {
+  get,
+  search
+}
+
+function get(country) {
+  const url = 'https://newsapi.org/v2/top-headlines';
+
   return fetch(`${url}?country=${country}&apiKey=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => data);
+}
+
+function search(searchTerm) {
+  const url = 'https://newsapi.org/v2/everything';
+  
+  return fetch(`${url}?q=${searchTerm}&apiKey=${API_KEY}`)
     .then(response => response.json())
     .then(data => data);
 }
