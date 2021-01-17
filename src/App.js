d8fa960ac905011bac059ada74fcf78100ba3fe6
Paul <paul@live-eo.com>
2021-01-17 17:04:56 +0100

    merged branch pagination

--- src/App.js
+++ src/App.js
@@ -8,22 +8,31 @@ const App = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [search, setSearch] = useState('');
 	const [country, setCountry] = useState('au');
-	const [articles, setArticles] = useState([]);
+  const [headlines, setHeadlines] = useState([]);
+  const [articles, setArticles] = useState([])
 
-  useEffect(() => {
-		loadHeadlines(country);
-	}, [country])
+  useEffect(() => fetchHeadlines(country), [country])
+  useEffect(() => splitArticlesByLimit(headlines), [headlines])
 
   const toggleMenu = () => setIsOpen(!isOpen);
   const searchArticles = (event) => setSearch(event.target.value);
-	const selectCountry = (event) => setCountry(event.target.value);
+  const selectCountry = (event) => setCountry(event.target.value);
 
-  const loadHeadlines = async() => {
+  const fetchHeadlines = async() => {
 		let data = await getHeadlines(country);
-		setArticles(data.articles)
-	}
+		setHeadlines(data.articles);
+  }
+  
+  const splitArticlesByLimit = (headlines) => {
+    if (headlines.length > 0) {
+      const limit = 10
+      const splitArticles = new Array(Math.ceil(headlines.length / limit))
+        .fill().map(_ => headlines.splice(0, limit));
+      setArticles(splitArticles)
+    }
+  }
 	
-  return (
+  return(
     <div className="App">
       <SidePanel 
         isOpen={isOpen}
@@ -33,8 +42,8 @@ const App = () => {
       />
       <MainContent 
         isOpen={isOpen}
-        toggleMenu={toggleMenu} 
         articles={articles}
+        toggleMenu={toggleMenu} 
       />
     </div>
   );
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import Article from '../Article/Article';
 import classes from './MainContent.module.css';
 
 const MainContent = ({ isOpen, toggleMenu, articles }) => {
+	const [index, setIndex] = useState(0);
+
+	const handleIndex = (index) => setIndex(index);
 
 	return(
     <div className={classes.container}>
-			{!isOpen && 
-				<div className={classes.menu} onClick={toggleMenu}>
-					<BurgerMenu />
+			<div className={classes.header}>
+				{!isOpen && 
+					<div className={classes.menu} onClick={toggleMenu}>
+						<BurgerMenu />
+					</div>
+				}
+				<div className={classes.pagination}>
+					<span>{`Page ${index + 1}`}</span>
+					<div>
+						{articles.length > 0 && articles.map((_, i) => {
+							if (index === i) return;
+							return(
+								<button 
+									key={i}
+									onClick={() => handleIndex(i)}
+								>{`Page ${i + 1}`}</button>
+							)
+						})}
+					</div>
 				</div>
-			}
+			</div>
 			<section>
-				{articles.length > 0 && articles.map((article, index) => {
+				{articles.length > 0 && articles[index].map((article, index) => {
 					return(
 						<Article 
 							key={index} 
